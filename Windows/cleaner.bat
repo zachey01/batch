a580e695cbc68dbedc4dc2f53fb9f1e21ded6088
zachey <63107653+zachey01@users.noreply.github.com>
2024-08-01 20:48:58 +0500

    Create cleaner.bat

---
+++ Windows/cleaner.bat
@@ -0,0 +1,43 @@
+@echo off
+
+rem Check If User Has Admin Privileges
+timeout /t 1 /nobreak > NUL
+openfiles > NUL 2>&1
+if %errorlevel%==0 (
+    echo Running..
+) else (
+    echo You must run an Admin
+)
+
+echo.
+
+del /s /f /q %WinDir%\Temp\*.*
+del /s /f /q %WinDir%\Prefetch\*.*
+del /s /f /q %Temp%\*.*
+del /s /f /q %AppData%\Temp\*.*
+del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.*
+del /s /f /q %SYSTEMDRIVE%\AMD\*.*
+del /s /f /q %SYSTEMDRIVE%\NVIDIA\*.*
+del /s /f /q %SYSTEMDRIVE%\INTEL\*.*
+rd /s /q %WinDir%\Temp
+rd /s /q %WinDir%\Prefetch
+rd /s /q %Temp%
+rd /s /q %AppData%\Temp
+rd /s /q %HomePath%\AppData\LocalLow\Temp
+rd /s /q %SYSTEMDRIVE%\AMD
+rd /s /q %SYSTEMDRIVE%\NVIDIA
+rd /s /q %SYSTEMDRIVE%\INTEL
+
+rem Recreate Empty Temporary Folders
+md %WinDir%\Temp
+md %WinDir%\Prefetch
+md %Temp%
+md %AppData%\Temp
+md %HomePath%\AppData\LocalLow\Temp
+
+echo.
+echo Windows Cleaned!
+echo.
+
+pause > NUL
+exit
